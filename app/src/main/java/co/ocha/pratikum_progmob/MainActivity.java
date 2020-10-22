e05f875010393bf708a72c6bc5df41887be2a85e
Gd Ocha <gdochadipa@gmail.com>
2020-10-22 16:02:41 +0800

    first

--- app/src/main/java/co/ocha/pratikum_progmob/MainActivity.java
+++ app/src/main/java/co/ocha/pratikum_progmob/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
---
+++ app/src/main/java/co/ocha/pratikum_progmob/remote/RetrofitClient.java
@@ -0,0 +1,4 @@
+package co.ocha.pratikum_progmob.remote;
+
+public class RetrofitClient {
+}
